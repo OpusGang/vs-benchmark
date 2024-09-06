cf87727d9e0ad01cb2edb205a0e69ab02ef0800c
NSQY <34185288+NSQY@users.noreply.github.com>
2024-09-07 05:39:19 +0930

    update readme, add example

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # vs-benchmark
-Vapoursynth benchmark utility
+Moderately extensive tool for benchmarking Vapoursynth functions en masse
+
+Limitations and considerations
+    - Memory usage is not GC'd between runs, so testing multiple functions returns bogus results 
+    - Short benchmarks may not accurately be accurately reported (psutil bug maybe)
+
+see example.py for usage
---
+++ example.py
@@ -0,0 +1,68 @@
+import itertools
+from vstools import vs, core
+from vsrgtools import box_blur
+from vsbenchmark import VSBenchmark, DisplayMetric
+
+_range = range(1, 3)
+
+def box_blur_generator() -> list[dict[str, int]]:
+    passes = _range
+    radii = _range
+    return [{'passes': p, 'radius': r} for p, r in itertools.product(passes, radii)]
+
+
+param_grid = {
+    'hpasses': list(_range),
+    'vpasses': list(_range),
+    'hradius': list(_range),
+    'vradius': list(_range),
+    'passes': list(_range),
+    'radius': list(_range)
+}
+
+param_mapping = {
+    "std boxblur": ['hpasses', 'vpasses', 'hradius', 'vradius'],
+    "avx boxblur": ['hpasses', 'vpasses', 'hradius', 'vradius'],
+    "zig boxblur": ['hpasses', 'vpasses', 'hradius', 'vradius'],
+    "zip boxblur": ['hpasses', 'vpasses', 'hradius', 'vradius'],
+    "jet boxblur": box_blur_generator
+}
+
+functions = {
+    "std boxblur": lambda x, params: core.std.BoxBlur(x, **params),
+    "avx boxblur": lambda x, params: core.box.Blur(x, **params),
+    "zig boxblur": lambda x, params: core.zboxblur.Blur(x, **params),
+    "zip boxblur": lambda x, params: core.vszip.BoxBlur(x, **params),
+    "jet boxblur": lambda x, params: box_blur(x, **params)
+}
+
+# I am generally of the thought that when testing function performance
+# it is best to use either a single thread or a controlled number of threads
+# There are too many potential issues when testing fast functions across
+# many threads. This is less of a problem when testing
+# compute heavy filters (BM3D) where thread vs FPS scaling is more linear.
+
+# Consider set_affinity:
+# core.set_affinity(threads=range(0, 32, 2), max_cache=22000)
+
+core.num_threads = 1
+
+benchmark = VSBenchmark(
+    functions=functions,
+    formats=[vs.GRAY16, vs.GRAYS],
+    resolutions=[(3840, 2160)],
+    length=100,
+    max_threads=None,
+    passes=4,
+    param_grid=param_grid,
+    param_mapping=param_mapping,
+)
+
+
+benchmark.run_benchmark()
+benchmark.print_results(DisplayMetric.TIME | DisplayMetric.FPS)
+benchmark.print_summary(DisplayMetric.ALL)
+
+# TODO
+# benchmark.write("data.csv")
+# benchmark.plot()
